9d0d624e076094fb27b9ebcbd2ae0939f24d49ae
Aarnav Sharma <aarnavs1008@gmail.com>
2026-06-25 10:17:02 +0530

    1. adding java.util.Arrays import and extension of .java forgot to add earlier

---
+++ LeetCode_Top_150/MajorityElement.java
@@ -0,0 +1,10 @@
+// 169. Majority Element
+
+import java.util.Arrays;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
